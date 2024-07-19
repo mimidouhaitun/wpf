018ca2ed51fea4d0db84b3d435b9dc42ce228921
huanghairong <huanghairong@canny-elevator.com>
2024-07-19 17:03:53 +0800

    To Do 项目

---
+++ MyToDo/MyToDo/Views/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+
+namespace MyToDo.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.btnMin.Click += (s, e) =>
+            {
+                this.WindowState = WindowState.Minimized;
+            };
+
+            this.btnMax.Click += (s, e) =>
+            {
+                if (this.WindowState == WindowState.Maximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+                }
+            };
+
+            this.btnClose.Click += (s, e) =>
+            {
+                this.Close();
+            };
+            //鼠标按下拖动，移动窗口
+            this.colorZone.MouseMove += (s, e) =>
+            {
+                if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
+                {
+                    this.DragMove();
+                }
+            };
+            //鼠标双击最大化或者还原
+            this.colorZone.MouseDoubleClick += (s, e) => {
+                if (this.WindowState == WindowState.Maximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+                }
+            };
+        }
+    }
+}
